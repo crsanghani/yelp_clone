f6de5ed82a3c7a77e5a5069f3742dbfc30a58e35
Chetan Sanghani <crsanghani@gmail.com>
2016-11-16 11:00:12 +0000

    corrected some spelling mistakes in the feature tests

--- spec/restaurants_feature_spec.rb
+++ spec/restaurants_feature_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'restaurants' do
 
-  context 'no restauranrs have been added' do
+  context 'no restaurants have been added' do
     scenario 'shuold display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'NOOO RESTAURANTS EVER!!!'
